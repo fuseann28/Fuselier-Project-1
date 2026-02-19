79b71d2a75e52deeec919b27577c11b7b2d6b3db
fuseann28 <fuseann28@gmail.com>
2026-02-19 00:16:06 -0600

    added light and dark mode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // JS scripts placed here
+const btn = document.querySelector("#theme-btn");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
+});
+ 
